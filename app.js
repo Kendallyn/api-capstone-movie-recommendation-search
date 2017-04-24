a4ab48353f7a2c15c807247aa99c1a623d2fb3e6
Kendallyn <Kendallynlyon@hotmail.com>
2017-04-24 10:23:59 -0700

    api parameters modified

--- app.js
+++ app.js
@@ -2,42 +2,72 @@ $(document).ready(function () {
     $("#searchForm").submit(function (event) {
         event.preventDefault();
         var userSearch = $("#search").val();
-        getSearchResults(userSearch);
+        if (userSearch.length == 0) {
+            alert("Please add movie title to search for it");
+        } else {
+            getSearchResults(userSearch);
+        }
+
     });
 
     function getSearchResults(userEntry) {
         //address from tastedive?
-        $.getJSON("https://tastedive.com/api/similar") {
-                q: userEntry,
-                type: 'movies',
-                info: 1,
-                limit: 18,
-                k: '267623-Thinkful-76WV9F3O',
-                callback: 'foo'
-            },
-            function (apiData) {
-                console.log(apiData);
-                if (apiData.pageInfo.totalResults == 0) {
-                    alert("No results found");
-                } else {
-                    displayResults(apiData.items);
-                }
-            });
 
-    function displayResults( ? ? ) {
+        var params = {
+            q: userEntry,
+            type: 'movies',
+            info: 1,
+            limit: 18,
+            k: '267623-Thinkful-76WV9F3O',
+            callback: 'foo'
+        };
+        var result = $.ajax({
+                /* update API end point */
+                url: "https://tastedive.com/api/similar",
+                data: params,
+                dataType: "jsonp",
+                /*set the call type GET / POST*/
+                type: "GET"
+            })
+            /* if the call is successful (status 200 OK) show results */
+            .done(function (output) {
+                /* if the results are meeningful, we can just console.log them */
+                //console.log(output.Similar.Results);
+                if (output.Similar.Results.length == 0) {
+                    alert("No results found for " + userEntry);
+                } else {
+                    displayResults(output.Similar.Results);
+                }
+            })
+            /* if the call is NOT successful show errors */
+            .fail(function (jqXHR, error, errorThrown) {
+                console.log(jqXHR);
+                console.log(error);
+                console.log(errorThrown);
+            });
+    }
+
+    function displayResults(movieApiResults) {
+        console.log(movieApiResults);
         var createHtmlList = "";
 
-        $.each( ? ? , function (Key, Value) {
+        $.each(movieApiResults, function (Key, Value) {
+            console.log(Value.Name);
+            console.log(Value.wTeaser);
+            console.log(Value.yUrl);
             createHtmlList += "<li>";
-            //space
-            createHtmlList += "<p>" + //videoArrayValue.snippet.title + "</p>";
-                createHtmlList += "<a href='https://tastedive.com/api/similar" + //videoArrayValue.id.videoId + "'>";
-                //shows movie description
-                //createHtmlList += "<img src='"
-                createHtmlList += "</a>";
+            createHtmlList += "<h2>" + Value.Name + "</h2>";
+            createHtmlList += "<a href='" + Value.yUrl + "' target='_blank'><img src='img/placeholder.jpg'></a>";
+            createHtmlList += "<p>" + Value.wTeaser + "</p>";
             createHtmlList += "</li>";
+            //space
+            //                        createHtmlList += "<p>" + videoArrayValue.snippet.title + "</p>";
+            //                        createHtmlList += "<a href='https://tastedive.com/api/similar" + videoArrayValue.id.videoId + "'>";
+            //                        createHtmlList += "<img src='";
+            //                        createHtmlList += "</a>";
+            //                        createHtmlList += "</li>";
         });
-        $("#searchResults ul").append(createHtmlList);
+        $("#searchResults ul").html(createHtmlList);
         console.log(createHtmlList);
     }
 });
